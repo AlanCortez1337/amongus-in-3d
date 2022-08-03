92fa685d89605c1869b9066202949c956f86de0a
AlanCortez1337 <acortez1337@csu.fullerton.edu>
2022-08-03 11:58:18 -0700

    changed favicon and title

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,10 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>AMOGUS IN 3D!!!</title>
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🔪</text></svg>"/>      
+      </Head>
       <div className={styles.amongus}>
         <Spline scene="https://prod.spline.design/W4B-zY9oge8eOo6m/scene.splinecode" />
       </div>
