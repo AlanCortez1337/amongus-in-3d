179a50da8446ce75178715c8135a748c3871d040
AlanCortez1337 <acortez1337@csu.fullerton.edu>
2022-08-03 14:56:42 -0700

    added a loading message when use first logs on

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
+import { useEffect, useState} from 'react';
 import Head from 'next/head'
-import Image from 'next/image'
 import Spline from '@splinetool/react-spline';
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
+  const [amongusLoaded, setAmongusLoaded] = useState(false);
+
+  useEffect(()=>{
+    setAmongusLoaded(true);
+    setTimeout(()=>{
+      setAmongusLoaded(false);
+    }, 5000)
+  },[])
+
   return (
     <div>
       <Head>
@@ -11,6 +20,7 @@ export default function Home() {
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🔪</text></svg>"/>      
       </Head>
       <div className={styles.amongus}>
+        {amongusLoaded && <p className={styles.loading}> <i>loading...</i> ((its becoming ebic!!!)) </p>}
         <Spline scene="https://prod.spline.design/W4B-zY9oge8eOo6m/scene.splinecode" />
       </div>
       <div className={styles.title}>
