f4c082949a9f515e9904c109b2c46418b8912ea9
AlanCortez1337 <acortez1337@csu.fullerton.edu>
2022-08-05 15:56:09 -0700

    added loading screen

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
-import React, { Suspense } from 'react';
-
-const Spline = React.lazy(() => import('@splinetool/react-spline'));
+import React, { Suspense, useState } from 'react';
+// const Spline = React.lazy(() => import());
 import Head from 'next/head'
 import styles from '../styles/Home.module.css';
 import SyncLoader from "react-spinners/SyncLoader";
+import Spline from '@splinetool/react-spline';
 
 export default function Home() {
+  const [loading, setLoading] = useState(true)
+  const [loadingMSG, setLoadingMSG] = useState("loading please wait...")
 
   return (
     <div className={styles.bg}>
@@ -14,9 +16,13 @@ export default function Home() {
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🔪</text></svg>"/>      
       </Head>
       <div className={styles.amongus}>
-        <Suspense fallback={<SyncLoader color={"#D66853"} loading={true} size={30} className={styles.loading}/>}>
-          <Spline scene="https://prod.spline.design/W4B-zY9oge8eOo6m/scene.splinecode" />
-        </Suspense>
+          {loading && 
+            <div className={styles.loading}>
+              <SyncLoader color={"#D66853"} loading={loading} size={10}/>
+              <p>{loadingMSG}</p>
+            </div>
+          }
+          <Spline scene="https://prod.spline.design/W4B-zY9oge8eOo6m/scene.splinecode" onStart={()=>{setLoading(false)}} onLoad={()=>{setLoadingMSG("almost there...")}}/>
       </div>
       <div className={styles.title}>
         <h1>AMOGUS IN 3D!!!!</h1>
